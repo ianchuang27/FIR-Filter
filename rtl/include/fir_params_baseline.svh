f144658b8253355267cb84e39d4c2e8396fb90b0
ianchuang27 <ian.chuang27@gmail.com>
2026-03-23 09:37:13 -0400

    Add files via upload

---
+++ rtl/include/fir_params_baseline.svh
@@ -0,0 +1,6 @@
+parameter int NTAPS      = 232,
+parameter int IN_W       = 16,
+parameter int COEFF_W    = 22,
+parameter int ACC_W      = 39,
+parameter int OUT_W      = 16,
+parameter int SHIFT_BITS = 21
